772accc9d82f28c73a5bb9abe3d90123b40b375a
Karol Moroz <karol@moroz.dev>
2025-04-18 23:00:55 +0800

    Initial commit

---
+++ table.h
@@ -0,0 +1,30 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+  char *chars;
+  int length;
+  uint32_t hash;
+} String;
+
+typedef struct {
+  String *key;
+  int value;
+} Entry;
+
+typedef struct {
+  int capacity;
+  int count;
+  Entry entries[];
+} Table;
+
+void initTable(Table *table);
+void freeTable(Table *table);
+bool tableSet(Table *table, String *key, int value);
+bool tableGet(Table *table, String *key, int *value);
+bool tableDelete(Table *table, String *key);
+
+#endif
